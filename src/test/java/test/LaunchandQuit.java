332ed3558a037444727fb91d94e457ea90a81e79
RaviPrakashReddy11 <raviprakashreddy60@gmail.com>
2024-12-21 18:23:37 +0530

    new

--- src/test/java/test/LaunchandQuit.java
+++ src/test/java/test/LaunchandQuit.java
@@ -3,7 +3,7 @@ package test;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,7 +13,7 @@ public class LaunchandQuit
 	@BeforeMethod
 	public void launch()
 	{
-		driver=new EdgeDriver();
+		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.in");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
